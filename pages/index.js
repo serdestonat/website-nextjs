333dd692fd934e6da4287e43517fe668d15d06c7
serdestonat <onat904@gmail.com>
2023-07-20 18:01:24 +0300

    update: tab logo

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
+          <link rel="icon" href="/hatchet.jpg" />
           <title>Home</title>
         </Head>
         <div className="index">
